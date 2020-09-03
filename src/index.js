ccd9435c4f4c5e9c82ea9c7bc2c64e14ade5580e
Der Meyer <simon.der.meyer@gmail.com>
2020-09-03 11:53:36 +0200

    Pixi App with black background.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Application } from 'pixi.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class PixiApp {
+    constructor() {
+        this.app = new Application({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+
+        this.create();
+    }
+
+    create() {
+        document.body.style.padding = '0';
+        document.body.style.margin = '0';
+
+        this.app.renderer.backgroundColor = 0x000000;
+
+        document.getElementById('root').appendChild(this.app.view);
+    }
+}
+
+new PixiApp();
